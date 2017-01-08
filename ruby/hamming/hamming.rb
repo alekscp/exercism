047480407c943308720ad4a123616daeddd0082d
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-01-08 14:32:38 -0500

    Refactor

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,8 +2,7 @@ module Hamming
   def self.compute(a, b)
     raise ArgumentError if a.length != b.length
 
-    a_split = a.split('')
-    b_split = b.split('')
+    a_split, b_split = a.split(''), b.split('')
 
     (0...a_split.length).map { |idx| a_split[idx] == b_split[idx] }.count(false)
   end
