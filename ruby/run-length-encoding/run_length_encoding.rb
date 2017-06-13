f8d9866261cb0c1bf91d5d2414f37997775c3137
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-06-13 21:53:36 +0200

    Raw implementation

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,24 +1,23 @@
-class RunLengthEncoding
+module RunLengthEncoding
   def self.encode(input)
-    new(input).run
-  end
-
-  def initialize(input)
-    @input = input
-  end
-
-  def run
-    grouped_characters.inject("") do |str, group|
+    input.scan(/((.)\2*)/).map(&:first).inject("") do |str, group|
       str << (group.length > 1 ? "#{group.length}#{group[0]}" : "#{group[0]}")
       str
     end
   end
 
-  private
-
-  attr_reader :input
-
-  def grouped_characters
-    @grouped_characters ||= input.scan(/((.)\2*)/).map(&:first)
+  def self.decode(input)
+    input.scan(/((\d*.)\2*)/).map(&:first).inject("") do |str, group|
+      if group.length > 1 && group[/\d*/].to_i > 0
+        str << group[/[^\d]/] * group[/\d*/].to_i
+      elsif group.length == 1
+        str << group
+      else
+        str << input
+      end
+      str
+    end
   end
 end
+
+module BookKeeping; VERSION = 3; end
