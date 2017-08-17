ce871b65b9581f93b1e4c4adece69c0460813fdc
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-08-17 18:05:38 +0200

    Refactor

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -2,9 +2,7 @@ module Binary
   def self.to_decimal(num)
     raise ArgumentError if num.match(/[^01]/)
 
-    reversed_digits = num.scan(/\d/).reverse
-
-    reversed_digits.map.with_index do |d, i|
+    num.scan(/\d/).reverse.map.with_index do |d, i|
       d.to_i * 2**i
     end.inject(:+)
   end
