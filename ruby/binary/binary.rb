9a2a456a6467a2f634ce551c29795c881a3f2a60
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-08-17 18:03:07 +0200

    Nake tests pass

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,9 +1,13 @@
 module Binary
   def self.to_decimal(num)
+    raise ArgumentError if num.match(/[^01]/)
+
     reversed_digits = num.scan(/\d/).reverse
 
     reversed_digits.map.with_index do |d, i|
-      d.to_i * 10**i
+      d.to_i * 2**i
     end.inject(:+)
   end
 end
+
+module BookKeeping; VERSION = 3; end
