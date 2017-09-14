faa2dd633c38be2a996b29e080fd0ee04f2e092f
Aleksandre Clapin-Pepin <aclapinpepin@gmail.com>
2017-09-13 20:38:27 -0400

    Move regexp to constant

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,10 +1,14 @@
 module PhoneNumber
+  PHONE_NUMBER_REGEX = Regexp.new('(\d{0,1})(\d{3})(\d{3})(\d{4})$')
+
   def self.clean(number)
     number.gsub!(/[^\d]/, '')
-    country_code, area_code, prefix, line_number = (number.match(/(\d{0,1})(\d{3})(\d{3})(\d{4})$/) || [])[1..-1]
+    country_code, area_code, prefix, line_number = (number.match(PHONE_NUMBER_REGEX) || [])[1..-1]
+
     return unless ['', '1'].include?(country_code)
     return if area_code[0] == '1'
     return unless prefix[/^[2-9]/]
+
     area_code + prefix + line_number
   end
 end
